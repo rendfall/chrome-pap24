a67183fff3315e9b3894c43bf88a6e460674b487
rendfall@gmail.com <rendfall@gmail.com>
2016-05-04 00:02:53 +0200

    Fix class toggling

--- src/scripts/panel.js
+++ src/scripts/panel.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $('.panel-container button').click(function (event) {
         event.preventDefault();
 
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
         var tab = $(this).attr('target');
 
         $('.page-container .page').not(tab).removeClass('active');
